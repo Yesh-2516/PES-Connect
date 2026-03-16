23b8e58a5012387776f9d3b24e29995b98ade087
Yeshaswinie D <Yeshaswinie.d@gmail.com>
2026-03-16 20:29:28 +0530

    Delete src directory

--- src/models/Interest.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const interestSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  eventId: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
-});
-
-export default mongoose.model("Interest", interestSchema);
